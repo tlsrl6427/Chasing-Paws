2ec7596ec14f54a5e5c9b33ed49a66236727c332
tlsrl6427 <tlsrl6427@naver.com>
2022-07-05 17:47:13 +0900

    SkillVo

---
+++ src/main/java/vo/SkillVo.java
@@ -0,0 +1,43 @@
+package vo;
+
+public class SkillVo {
+
+	int s_idx;
+	String s_name;
+	String s_category;
+	int s_turn;
+	int c_idx;
+	
+	public int getS_idx() {
+		return s_idx;
+	}
+	public void setS_idx(int s_idx) {
+		this.s_idx = s_idx;
+	}
+	public String getS_name() {
+		return s_name;
+	}
+	public void setS_name(String s_name) {
+		this.s_name = s_name;
+	}
+	public String getS_category() {
+		return s_category;
+	}
+	public void setS_category(String s_category) {
+		this.s_category = s_category;
+	}
+	public int getS_turn() {
+		return s_turn;
+	}
+	public void setS_turn(int s_turn) {
+		this.s_turn = s_turn;
+	}
+	public int getC_idx() {
+		return c_idx;
+	}
+	public void setC_idx(int c_idx) {
+		this.c_idx = c_idx;
+	}
+	
+	
+}
